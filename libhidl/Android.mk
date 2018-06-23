f023566f739eff4f85b18b9b5e429baaf70f187e
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    Amber: Build separate system and vendor hidl libs
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e

--- libhidl/Android.mk
+++ libhidl/Android.mk
@@ -14,14 +14,15 @@
 # limitations under the License.
 
 include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libhidlbase
+LOCAL_SHARED_LIBRARIES := libhidltransport
 LOCAL_MODULE := android.hidl.base@1.0
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libhidlbase
+LOCAL_SHARED_LIBRARIES := libhidltransport
 LOCAL_MODULE := android.hidl.manager@1.0
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -29,9 +30,17 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libhidlbase
+LOCAL_SHARED_LIBRARIES := libhidltransport
 LOCAL_MODULE := android.hidl.base@1.0_system
 LOCAL_INSTALLED_MODULE_STEM := android.hidl.base@1.0.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SHARED_LIBRARIES := libhidltransport
+LOCAL_MODULE := android.hidl.manager@1.0_system
+LOCAL_INSTALLED_MODULE_STEM := android.hidl.manager@1.0.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)

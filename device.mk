26aca0e55a71a325568588fccc4b4d1ebac3371b
Joey <joey@lineageos.org>
2019-01-13 10:21:11 +0100

    Amber: Build Trust HAL
    Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df
    Signed-off-by: Joey <joey@lineageos.org>

--- device.mk
+++ device.mk
@@ -333,6 +333,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.amber
 
+# Lineage hardware
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm

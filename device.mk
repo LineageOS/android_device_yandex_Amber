efdc1a2f200c2557e41eefcde7d664e6b231923e
Ramjee Singh <ramjee@codeaurora.org>
2019-07-03 15:03:56 +0530

    Amber: Enable frame ctl for aac codec format
    Change-Id: Id43ab138deb892e564ed710e4dd5c9b644852f44

--- device.mk
+++ device.mk
@@ -206,6 +206,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.parser.ip.buffer.size=262144 \
     vendor.audio.flac.sw.decoder.24bit=true \
     persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
+    persist.vendor.bt.aac_frm_ctl.enabled=true \
     vendor.audio.use.sw.alac.decoder=true \
     vendor.audio.use.sw.ape.decoder=true \
     vendor.audio.hw.aac.encoder=true \

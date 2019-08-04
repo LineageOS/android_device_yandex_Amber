d03647ad3d19376b9e56b1aa2291081d8ae0a031
vm03 <vasy@vasy.ru>
2019-08-04 11:21:49 +0300

    Amber: fp: use ueventd.rc
     instead of chown in init
     partially revert 0dffd818b

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -115,11 +115,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := elan_device.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/elan_device.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
-include $(BUILD_PREBUILT)

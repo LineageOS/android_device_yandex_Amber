0ac963c1f81ed1b15b065fef7dca8746c1ed96f6
Bruno Martins <bgcngm@gmail.com>
2019-09-25 23:00:35 +0100

    Amber: Kill framework compatibility matrix with fire
    This thing is not mandatory at all and only demands extra work by having
    to keep it up-to-date whenever new interfaces are added to the device manifest
    or simply uprev'ed.
    
    Change-Id: I7c5485f201efde86ff1b277951e7faebf8b80437

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,7 +156,6 @@ TARGET_PROVIDES_LIBLIGHT := true
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/configs/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/framework_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/vendor_framework_compatibility_matrix.xml
 
 # Media
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
